a7add289306ac467abf3eebe4a4853761e33cf34
lauradriscoll <lndriscoll@gmail.com>
2016-04-25 18:00:30 -0400

    working demo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # welcome2miami
+
 This is an art collaboration between two people. It will be really cool.
+
+Things we want:
+- Filter to pastel colors (filtered based on time of day)
+- Overlaid neon signs that link to specific projects
---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+  Renders a StreetView panorama in ThreeJS.
+ */
+
+var THREE = require('three')
+var equirect = require('google-panorama-equirectangular')
+var panorama = require('google-panorama-by-location')
+var createOrbitViewer = require('three-orbit-viewer')(THREE)
+
+var app = createOrbitViewer({
+  clearColor: 0xffffff,
+  clearAlpha: 1.0,
+  fov: 45,
+  position: new THREE.Vector3(0, 0, -0.1)
+})
+
+var texture = new THREE.Texture()
+texture.minFilter = THREE.LinearFilter
+texture.generateMipmap = false
+
+// transparent canvas to start (white)
+var canvas = document.createElement('canvas')
+texture.needsUpdate = true
+texture.image = canvas
+
+// add a double-sided sphere
+var geo = new THREE.SphereGeometry(1, 84, 84)
+var mat = new THREE.MeshBasicMaterial({
+  map: texture,
+  side: THREE.DoubleSide
+})
+var sphere = new THREE.Mesh(geo, mat)
+app.scene.add(sphere)
+
+// flip the texture along X axis
+sphere.scale.x = -1
+
+// a street view in Tokyo
+var location = [25.7794081,-80.1308771]
+panorama(location, function (err, result) {
+  if (err) throw err
+
+  // load the equirectangular image
+  equirect(result.id, {
+    tiles: result.tiles,
+    canvas: canvas,
+    crossOrigin: 'Anonymous',
+    zoom: 4
+  })
+    .on('complete', function (image) {
+      texture.needsUpdate = true
+    })
+    .on('progress', function (ev) {
+      texture.needsUpdate = true
+    })
+})
